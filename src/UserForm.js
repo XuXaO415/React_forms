4a5cfcdc8cfd937c73c954c46f03cf43f90b98bd
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-12 20:57:47 -0700

    Added controlled components

---
+++ src/UserForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const UserForm = () => {
+    return (
+        <form>
+            <input type="text" placeholder="username" />
+            <button>Add Me to list!</button>
+        </form>
+    )
+}
+
+export default UserForm;
