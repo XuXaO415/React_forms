28195a50fe8bc52b2f10397b76ad9f48ce1d1fee
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-05-12 22:05:53 -0700

    Added React States

--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -2,22 +2,74 @@ import React, { useState } from "react";
 
 
 const UserForm = () => {
-    const[username, setUsername] = useState("FunnyApple");
-    const handleChange = (e) => {
-       setUsername(e.target.value);
-        // console.log(e.target.value)
+    // const[username, setUsername] = useState("");
+    // const [email, setEmail] = useState("");
+    const initialState = {
+        username: "",
+        email: "",
+        password: ""
     }
+    
+    const[formData, setFormData] = useState(initialState)
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        // console.log(e.target.name);
+        // console.log(e.target.value);
+        setFormData(data => ({
+            ...data,
+            [name]: value
+        }))
+    }
+
+    // const handleUsernameChange = (e) => {
+    //    setUsername(e.target.value);
+    //     // console.log(e.target.value)
+    // }
+    // const handleEmailChange = (e) => {
+    //     setEmail(e.target.value)
+    // }
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Create user, ${username}`)
-        setUsername("")
+        const {username, email, password} = formData;
+        alert(`Create user, ${username} with email ${email} & ${password}`)
+        setFormData(initialState)
     }
+
     return ( 
     <form onSubmit={handleSubmit}>
-        <input type = "text" placeholder = "username" value={username} onChange={handleChange}/>
-        <button onClick={handleSubmit} > Add Me to list! </button>
+    <label htmlFor="username">Username</label>
+        <input
+        id="username"
+        type="text"
+        name="username"
+        placeholder="username"
+        value={formData.username} 
+        onChange={handleChange}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+        type="email" 
+        name="email"
+        placeholder="email" 
+        id="email" 
+        value={formData.email} 
+        onChange={handleChange}/>
+     
+
+        <label htmlFor="password">Password</label>
+        <input
+        type="password" 
+        name="password"
+        placeholder="password" 
+        id="email" 
+        value={formData.password} 
+        onChange={handleChange}/>
+        <button  > Add Me to list! </button>
     </form >
     )
 }
+
 
 export default UserForm;
